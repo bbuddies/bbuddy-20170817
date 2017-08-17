c69b5e744c16a54d972063956876b13e14098f32
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-08-17 14:32:15 +0800

    add budget: controller now get the submitted budget from ui

--- src/main/java/com/odde/bbuddy/budget/BudgetController.java
+++ src/main/java/com/odde/bbuddy/budget/BudgetController.java
@@ -1,13 +1,26 @@
 package com.odde.bbuddy.budget;
 
+import com.odde.bbuddy.budget.domain.Budget;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import static java.lang.System.out;
 
 @Controller
+@Slf4j
 public class BudgetController {
 
     @GetMapping("/budgets/add")
     public String addBudget() {
+        return "/budgets/add";
+    }
+
+    @PostMapping("/budgets/add")
+    public String submitAddBudget(Budget budget) {
+        out.println("month: " + budget.getMonth());
+        out.println("amount: " + budget.getAmount());
         return "/budgets/add";
     }
 
---
+++ src/main/java/com/odde/bbuddy/budget/domain/Budget.java
@@ -0,0 +1,12 @@
+package com.odde.bbuddy.budget.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Budget {
+
+    private String month;
+    private int amount;
+}
